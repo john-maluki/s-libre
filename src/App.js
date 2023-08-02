255f005ac4ffb9ab734c7ddb4a64ed0d3078c26d
john <johnmaluki58@gmail.com>
2023-08-02 19:39:38 +0300

    Enable login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { useState } from "react";
 import "./App.css";
+import { AuthContext } from "./contexts/AuthContext";
 import Layout from "./layouts/Layout";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <Layout />;
+  const [authUser, setAuthUser] = useState(null);
+
+  const login = (userCredetials) => {
+    fetchUserFromServer(userCredetials.username, userCredetials.password);
+  };
+
+  const fetchUserFromServer = (username, password) => {
+    fetch(
+      `http://localhost:4000/users?username=${username}&password=${password}`
+    )
+      .then((resp) => resp.json())
+      .then((users) => {
+        if (users.length === 1) {
+          return setAuthUser(users[0]);
+        } else {
+          return setAuthUser(null);
+        }
+      });
+  };
+
+  return authUser ? (
+    <AuthContext.Provider value={authUser}>
+      <Layout />
+    </AuthContext.Provider>
+  ) : (
+    <LoginPage login={login} />
+  );
 }
 
 export default App;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+const LoginPage = ({ login }) => {
+  const [userCredetials, setUserCredetials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(userCredetials);
+  };
+
+  const handleOnChange = (e) => {
+    const { value, name } = e.target;
+    setUserCredetials((userCredetials) => ({
+      ...userCredetials,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-card">
+        <div className="login-text">
+          <h1>Login</h1>
+        </div>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <div className="form-control">
+              <i className="fa fa-user-o" aria-hidden="true"></i>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Type your username"
+                onChange={handleOnChange}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <div className="form-control">
+              <i className="fa fa-unlock-alt" aria-hidden="true"></i>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Type your password"
+                onChange={handleOnChange}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="LOGIN" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
