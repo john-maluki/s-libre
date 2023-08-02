b739c01ede58a44e914b9d07b4ff2397bb9e4fc9
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-08-02 12:43:44 -0400

    fetch topics from the server

--- src/components/topics/Topics.js
+++ src/components/topics/Topics.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import TopicCard from "./TopicCard";
 
 const Topics = () => {
+  const [topics, setTopics] = useState([]);
+  
+  const fetchTopicsFromServer = () => {
+    fetch(`http://localhost:4000/topics`)
+      .then((resp) => resp.json())
+      .then((topics) => setTopics(topics));
+  };
+
+  useEffect(() => {
+    fetchTopicsFromServer();
+  }, []);
+
+  const topicList = topics.map((topic) => <TopicCard key={topic.id} topic={topic} />);
+
   return (
     <>
-      <TopicCard />
+    {topicList}
     </>
   );
 };
