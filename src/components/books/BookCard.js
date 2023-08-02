6f37d13eb2c164971356e7bcb0b774afcfbb7e1c
Tracy <tracy.kagiri@moringa.student.com>
2023-08-02 13:52:41 +0300

    fetch

--- src/components/books/BookCard.js
+++ src/components/books/BookCard.js
@@ -1,52 +1,48 @@
 import React from "react";
 
-const BookCard = () => {
+const BookCard = ({book}) => {
   return (
-    <div class="user-card">
-      <div class="user-card__profile">
+    <div className="user-card">
+      <div className="user-card__profile">
         <img
-          class="img-profile"
+          className="img-profile"
           src="https://img.freepik.com/free-photo/cheerful-curly-business-girl-wearing-glasses_176420-206.jpg?size=626&amp;ext=jpg&amp;ga=GA1.1.56092171.1688454543&amp;semt=ais"
           alt="Joy Wagere"
         />
       </div>
-      <div class="user-card__details">
-        <div class="user-card__header">
-          <h1 class="user-card__title">
-            Clean Architecture: A Craftsman's Guide to Software Structure and
-            Design
+      <div className="user-card__details">
+        <div className="user-card__header">
+          <h1 className="user-card__title">
+            {book.title}
+
           </h1>
         </div>
-        <p class="user-card__description">
-          Clean architecture is a software design philosophy that separates the
-          elements of a design into ring levels. An important goal of clean
-          architecture is to provide developers with a way to organize code in
-          such a way that it encapsulates the business logic but keeps it
-          separate from the delivery mechanism
+        <p className="user-card__description">
+         {book.book_description}
         </p>
         <img
-          class="user-card__img"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGDULhOY_Xw1fSzhcxycR2hn5ifI07df0N9w&usqp=CAU"
+          className="user-card__img"
+          src={book.book_pic}
           alt="Joy Wagere"
         />
-        <div class="user-card__footer">
-          <div class="user-card__expression">
+        <div className="user-card__footer">
+          <div className="user-card__expression">
             <i
-              class="fa fa-thumbs-up user-card__user-expression-icon"
+              className="fa fa-thumbs-up user-card__user-expression-icon"
               title="Follow"
               id="2"
             ></i>
-            <h3 class="user-card__expression-number">
-              Likes <span>40</span>
+            <h3 className="user-card__expression-number">
+              Likes <span>{book.likes.length}</span>
             </h3>
           </div>
-          <div class="user-card__expression">
+          <div className="user-card__expression">
             <i
-              class="fa fa-user user-card__user-expression-icon"
+              className="fa fa-user user-card__user-expression-icon"
               title="Follow"
             ></i>
-            <h3 class="user-card__expression-number">
-              Followers <span>150</span>
+            <h3 className="user-card__expression-number">
+              Followers <span>{book.followers.length}</span>
             </h3>
           </div>
         </div>
--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import BookCard from "./BookCard";
 
 const Books = () => {
-  return <BookCard />;
+  const[books,setBooks]=useState([])
+  const fetchBooks=()=> {
+    fetch("http://localhost:4000/books")
+    .then((response)=>response.json())
+    .then((books)=>setBooks(books))
+
+  }
+  useEffect(()=>{
+    fetchBooks()
+  },[])
+  const booksLists=books.map((book)=><BookCard key={book.id}book={book} />)
+  return <>{booksLists}</>
 };
 
 export default Books;
