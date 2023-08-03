026d206b756fca59a1f89502eb85ec9762db60d0
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-08-03 10:30:53 -0400

    fetch topics comments from the server

--- src/pages/TopicsPage.js
+++ src/pages/TopicsPage.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import RightMainBar from "../components/RightMainBar";
 import Topics from "../components/topics/Topics";
+import {MAIN_DOMAIN } from "../utils/constants"
 
 const TopicsPage = () => {
+  const [topics, setTopics] = useState([]);
+  const [topicComments, setTopicComents] =useState([]); 
+  const fetchTopicsCommentsFromServer = () => {
+    fetch(`${ MAIN_DOMAIN }/topics_comments`)
+      .then((resp) => resp.json())
+      .then((topics_comments) => setTopics(topics_comments));
+  };
+
+  const fetchTopicsFromServer = () => {
+    fetch(`${ MAIN_DOMAIN }/topics`)
+      .then((resp) => resp.json())
+      .then((topics) => setTopics(topics));
+  };
+
+  useEffect(() => {
+    fetchTopicsFromServer();
+  }, []);
+
   return (
     <>
       <section class="main__content-pane">
