12cdeede77e82cabae56e2f970c53bca7a2ed7e6
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-08-03 12:03:10 -0400

    fetch books comments from the server

--- src/pages/BooksPage.js
+++ src/pages/BooksPage.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import RightMainBar from "../components/RightMainBar";
 import Books from "../components/books/Books";
+import {MAIN_DOMAIN } from "../utils/constants"
 
 const BooksPage = () => {
+  const [books, setBooks] = useState([]);
+  const [topicBooks, setBookComents] =useState([]); 
+  const fetchBooksCommentsFromServer = () => {
+    fetch(`${ MAIN_DOMAIN }/books_comments`)
+      .then((resp) => resp.json())
+      .then((books_comments) => setBooks(books_comments));
+  };
+
+  const fetchBooksFromServer = () => {
+    fetch(`${ MAIN_DOMAIN }/books`)
+      .then((resp) => resp.json())
+      .then((books) => setBooks(books));
+  };
   return (
     <>
       <section className="main__content-pane">
