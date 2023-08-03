9b3523da6b384aad380cca38ce1d6a2aa63c37a8
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-03 14:29:05 +0300

    removed unnecessary code

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SignupPage.css";
 
@@ -77,10 +77,6 @@ const SignupPage = () => {
     setFormErrors(errors);
     return errors;
   };
-
-  useEffect(() => {
-    formValidation();
-  }, [formData]);
 
   return (
     <div className="signup-body">
